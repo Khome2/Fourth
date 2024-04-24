8c4759e4d37bdd98e5d461c9f6b3cd766bc4f212
Daniel Khome <166134592+Khome2@users.noreply.github.com>
2024-04-24 13:20:40 +0100

    Update time.js

--- Analog Clock/time.js
+++ Analog Clock/time.js
@@ -4,6 +4,7 @@ setInterval(function()  {
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
 
-    const time = '${hours}:$'
-    {minutes.toString().padStart(2, '0')}:$'
+    const time = `${hours}:$
+    {minutes.toString().padStart(2, '0')}:$
+    {seconds.toString().padStart(2, '0')}`;
 }, interval);
