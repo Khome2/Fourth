0ace59093cf881ba8f3e9b7c4488108177b08174
Daniel Khome <166134592+Khome2@users.noreply.github.com>
2024-04-24 13:07:26 +0100

    Add files via upload

---
+++ Analog Clock/time.js
@@ -0,0 +1,9 @@
+setInterval(function()  {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const time = '${hours}:$'
+    {minutes.toString().padStart(2, '0')}:$'
+}, interval);
